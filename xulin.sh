718d05835df63a20ff561deb93898158e7201966
Wang Gonging <wanggonging@gmail.com>
2018-01-22 05:04:14 +0800

    xulin.sh

---
+++ xulin.sh
@@ -0,0 +1,41 @@
+﻿c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-01 谨以此诗表达 对所有被中共当局 失踪者的思念 2014年4月23日" ./d xulin1诗 https://www.youtube.com/watch?v=bi3dL9gHL6M
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-02 站在正义这一边 2014年4月23日" ./d xulin2站在正义这一边 https://www.youtube.com/watch?v=G_cXBa1-CC8
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-03 就是现在 2014年4月24日" ./d xulin3就是现在 https://www.youtube.com/watch?v=hGzcKeZXIoU
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-04 林昭之歌 2014年4月26日" ./d xulin4林昭之歌 https://www.youtube.com/watch?v=q7abUmfcXCY
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-05 杨佳之歌 2014年5月11日" ./d xulin5杨佳之歌 https://www.youtube.com/watch?v=FLfk9YiXe0s
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-06 一人一票 2014年5月18日" ./d xulin6一人一票 https://www.youtube.com/watch?v=s32ViFobloQ
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-07 维护专制的狗 2014年5月18日" ./d xulin7维护专制的狗 https://www.youtube.com/watch?v=l0ZVqgkQwfI
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-08 我是香港一份子 2014年6月25日" ./d xulin8我是香港一份子 https://www.youtube.com/watch?v=98GKP7UB3Kg
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-09 纽约纪念六四 活动片段 2014年7月5日" ./d xulin9纽约纪念六四 https://www.youtube.com/watch?v=SkrlOe1seOE
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-10 阿弥陀佛歌 2014年9月22日" ./d xulin10 https://www.youtube.com/watch?v=9VhEdNAXgXE
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-11 新版《站在正义这一边》清晰 2014年10月28日" ./d xulin11新版《站在正义这一边》清晰 https://www.youtube.com/watch?v=n6vVr6aP4bg
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-12 我在去监狱的路上 2014年12月4日" ./d xulin12我在去监狱的路上 https://www.youtube.com/watch?v=3TPp98J3tZQ
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-13 庆幸 2015年1月8日" ./d xulin13庆幸 https://www.youtube.com/watch?v=OW3MAjGcCYo
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-14 我在自己的祖国流亡 2015年3月15日" ./d xulin14我在自己的祖国流亡 https://www.youtube.com/watch?v=dzwU9Y1kKs4
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-15 自由无价 2015年4月27日" ./d xulin15自由无价 https://www.youtube.com/watch?v=FFjZBz2dk3Q
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-16 一人一票精制版 2015年6月21日" ./d xulin16 https://www.youtube.com/watch?v=xUgitLBMx4c
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-17 正义律师之歌 2015年8月1日" ./d xulin17 https://www.youtube.com/watch?v=QRp9CrUbBIg
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-18 还原真相 2015年8月3日" ./d xulin18还原真相 https://www.youtube.com/watch?v=-7z_WUE_pQs
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-19 五毛狗之歌 2015年8月19日" ./d xulin19五毛狗之歌 https://www.youtube.com/watch?v=cTs_GQsaSlo
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-20 真的民主 2015年9月19日" ./d xulin20真的民主 https://www.youtube.com/watch?v=gyGr9sxHp_I
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-21 请叫我的真名 纪功碑 2015年10月20日" ./d xulin21请叫我的真名纪功碑 https://www.youtube.com/watch?v=EvX0TibHK4k
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-22 砍头也不回头 2015年11月9日" ./d xulin22砍头也不回头 https://www.youtube.com/watch?v=sUmxBYDjw4g
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-23 徐琳被传唤自述视频 2015年12月17日" ./d xulin23徐琳被传唤自述视频 https://www.youtube.com/watch?v=AaiQcr-JPI0
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-24 民主大潮 2016年1月11日" ./d xulin24民主大潮 https://www.youtube.com/watch?v=3y4WHAaouMg
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-25 民主大潮清晰 2016年1月12日" ./d xulin25民主大潮清晰 https://www.youtube.com/watch?v=MnT_g8V5YTI
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-26 坦克人 2016年1月26日" ./d xulin26坦克人 https://www.youtube.com/watch?v=z8A623aImt8
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-27 坦克人清晰版 2016年1月26日" ./d xulin27坦克人清晰版 https://www.youtube.com/watch?v=ym88UOz5ZC8
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-28 坐牢的姐妹兄弟 2016年2月7日" ./d xulin28坐牢的姐妹兄弟 https://www.youtube.com/watch?v=jr628hBHyhc
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-29 救救孩子 2016年4月7日" ./d xulin29救救孩子 https://www.youtube.com/watch?v=cLQ1ZOfUTmc
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-30 雷洋之死歌曲 2016年5月19日" ./d xulin30雷洋之死歌曲 https://www.youtube.com/watch?v=9i6r0anCn0s
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-31 站在正义这边新版 2016年5月28日" ./d xulin30站在正义这边新版 https://www.youtube.com/watch?v=VcWETqr26MQ
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-32 正义军歌 2016年7月13日" ./d xulin30正义军歌 https://www.youtube.com/watch?v=WPpMyirMG9M
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-33 神秘的巴拿马 2016年11月17日" ./d xulin31神秘的巴拿马 https://www.youtube.com/watch?v=MIVkAOtMj0w
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-34 玫瑰花开 2017年3月12日" ./d xulin32玫瑰花开 https://www.youtube.com/watch?v=oxgK0t2sVGw
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-35 推特党之歌 2017年5月26日" ./d xulin33推特党之歌 https://www.youtube.com/watch?v=HjhuBM6zsvI
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-36 放下你的枪 2017年6月2日" ./d xulin34放下你的枪 https://www.youtube.com/watch?v=7UYoLLUbozw
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-37 秘密关押的人权律师 2017年7月5日" ./d xulin35秘密关押的人权律师 https://www.youtube.com/watch?v=e1ixo2TyamI
+c=30 fps=25 speed=1 n=1 ss=0 S1="徐琳全集-38 我该如何挽救 2017年7月12日" ./d xulin30我该如何挽救 https://www.youtube.com/watch?v=_vZnN0EaRps
+
+
+
