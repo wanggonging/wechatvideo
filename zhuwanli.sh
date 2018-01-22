7adf1a7659cf886dec576e7246a35c701f7568aa
Wang Gonging <wanggonging@gmail.com>
2018-01-23 00:03:59 +0800

    zhuwanli.sh

---
+++ zhuwanli.sh
@@ -0,0 +1,25 @@
+
+#
+# 朱万利
+#
+
+#fps=10 speed=1 n=1 ss=0 S1="朱万利01 谈郭文贵 一个人的奴隶大起义 2017.7.27" ./d zwl01 https://www.youtube.com/watch?v=UV_2B3DfbBg
+#vv=7 fps=10 speed=1 n=1 ss=0 S1="朱万利02 力挺郭文贵 朱万利女士的反叛人生 2017.8.1" ./d zwl02 https://www.youtube.com/watch?v=wZ0f96u8oFY
+#fps=10 speed=1 n=1 ss=0 S1="朱万利03 全民参加 以郭文贵为主导的 民主大革命 2017.8.9" ./d zwl03 https://www.youtube.com/watch?v=rgGXKAGRy6M
+#fps=10 speed=1 n=1 ss=0 S1="朱万利04 请中共所谓的 “低端人口” 围观郭文贵推特！ 2017.8.16" ./d zwl04  https://www.youtube.com/watch?v=NbBAn5m7FOA
+#fps=10 speed=1 n=1 ss=0 S1="朱万利05 力挺郭文贵 活摘器官还要走多久(上) 2017.9.5" ./d zwl05 https://www.youtube.com/watch?v=kXEfSbKJ5sc
+#fps=10 speed=1 n=1 ss=0 S1="朱万利06 力挺郭文贵 活摘器官还要走多久(下) 2017.9.11" ./d zwl06 https://www.youtube.com/watch?v=H9rW55zdWeA
+#fps=10 speed=1 n=1 ss=0 S1="朱万利07 谈王立军的 活人人体实验 2017.9.16" ./d zwl07 https://www.youtube.com/watch?v=au0HgcbYvBo
+#fps=10 speed=1 n=1 ss=0 S1="朱万利08 中共滥用“红通” 迫害郭文贵迫害人权 2017.10.2" ./d zwl08 https://www.youtube.com/watch?v=usIeuI7d7so
+#fps=10 speed=1 n=1 ss=0 S1="朱万利09 谈蚁力神集资诈骗案 2017.11.12" ./d zwl09 https://www.youtube.com/watch?v=tvbsYP2QqEM
+#fps=10 speed=1 n=1 ss=0 S1="朱万利10 叙述《天堂文件》的 相关内容\\\\N2017.11.18" ./d zwl10 https://www.youtube.com/watch?v=dUqnx-WiNnY
+#vv=9 fps=10 speed=1 n=1 ss=0 S1="朱万利11 中共按计划行事 驱逐低端人口贫民运动 明镜电视《法治与社会》第82期 2017.11.27" ./d zwl11 https://www.youtube.com/watch?v=fwRTzqEyy5I
+#fps=10 speed=1 n=1 ss=0 S1="朱万利12 非法滞留中国的非裔人群 现状和问题\\\\N2017.12.7" ./d zwl12 https://www.youtube.com/watch?v=ZtsBHZAQx1M
+#fps=10 speed=1 n=1 ss=0 S1="朱万利13 十问中共 民众的生存权在哪\\\\N2017.12.10" ./d zwl13 https://www.youtube.com/watch?v=UQfIja6Im8g
+#fps=10 speed=1 n=1 ss=0 S1="朱万利14 谈万里大造林集资诈骗案\\\\N2017.12.18" ./d zwl14 https://www.youtube.com/watch?v=jnGWPpY6opk
+#fps=15 speed=1 n=1 ss=0 S1="朱万利15 中共征收遗产税 就是对民众财富 的掠夺 2018.01.01" ./d zwl0101 https://www.youtube.com/watch?v=PdLQrbMjnfQ
+fps=10 speed=1 n=1 ss=0 S1="{\\\\fs(50)}朱万利16 看看温家宝家族的财富 （上） 2018.01.03 {\\\\fs(30)}抱歉有二处口误 戴梦得宝石公司 有时说成戴得利宝石公司 女富豪段伟红有时说成段红伟" ./d zwl16 https://www.youtube.com/watch?v=peFtOwQ6A5Y
+fps=10 speed=1 n=1 ss=0 S1="朱万利17 看看温家宝家族 的财富(下)\\\\N2018.01.08" ./d zwl17 https://www.youtube.com/watch?v=pbRm9WNupO0
+fps=10 speed=1 n=1 ss=0 S1="朱万利18 这是2017.09.17 评论六四大屠杀\\\\N视频的节选\\\\N2018.01.11" ./d zwl18 https://www.youtube.com/watch?v=iZ_LzuOymck
+fps=10 speed=1 n=1 ss=0 S1="朱万利19 为什么江胡温 被邓小平 选定为接班人 2018.01.14" ./d zwl19 https://www.youtube.com/watch?v=kxV-lHPA7aM
+fps=10 speed=1 n=1 ss=0 S1="朱万利20 逃港潮 历史大悲剧 人民用脚投票\\\\N2018.01.21" ./d zwl20 https://www.youtube.com/watch?v=B34FJouxCk4
