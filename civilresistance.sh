e5ef40315cd7c38c94b1d233fa5d82d29614b320
Wang Gonging <wanggonging@gmail.com>
2018-01-22 12:59:07 +0800

    civilresistance.sh

---
+++ civilresistance.sh
@@ -0,0 +1,22 @@
+
+#
+# 街头革命
+#
+
+fps=25 speed=1.3 n=1 ss=0 S1="{\\\\fs(49)}《公民抵抗初探》 普通民众如何采取 公民抵抗 打败强大的统治者? 公民抵抗如何开始? 怎样组织抗议活动?" ./d cv h5ttps://www.youtube.com/watch?v=ficH9nLPZiE
+fps=10 speed=1 n=2 ss=0 S1="1986纪录片\\\\N菲律宾二月革命 英文中字" ./d prev1986 https://www.youtube.com/watch?v=BWQHSJJ8OyE
+fps=10 speed=1 n=1 ss=0 S1="2002纪录片 《推翻独裁者》-1 中文版" ./d howto-1 https://www.youtube.com/watch?v=P4H5Vr1rWbM
+fps=10 speed=1 n=1 ss=0 S1="2002纪录片 《推翻独裁者》-2 中文版" ./d howto-2 https://www.youtube.com/watch?v=LSzUGZpgVrY
+fps=10 speed=1 n=1 ss=0 S1="2002纪录片 《推翻独裁者》-3 中文版" ./d howto-3 https://www.youtube.com/watch?v=v-NURefGL54
+fps=10 speed=1 n=4 ss=0 S1="吉恩·夏普2011纪录片 《怎样开始一场革命？》 中文字幕" ./d rev https://www.youtube.com/watch?v=xa1oM7QYnIU
+fps=25 speed=1 n=1 ss=0 S1="《更强大的力量》\\\\N(一) 印度和纳什维尔1 中文版" ./d nv-a1 https://www.youtube.com/watch?v=Pt-Ko8lHQuM
+fps=25 speed=1 n=1 ss=0 S1="《更强大的力量》\\\\N(一) 印度和纳什维尔2 中文版" ./d nv-a2 https://www.youtube.com/watch?v=dfX9ZAhnChM
+fps=25 speed=1 n=1 ss=0 S1="《更强大的力量》\\\\N(一) 印度和纳什维尔3 中文版 " ./d nv-a3  https://www.youtube.com/watch?v=GkouoNKswNk
+fps=25 speed=1 n=1 ss=120 S1="《更强大的力量》\\\\N(二) 波兰和南非-1 中文版" ./d nv-b1 https://www.youtube.com/watch?v=jN6Cjc-1sSg
+fps=10 speed=1 n=1 ss=0 S1="《更强大的力量》\\\\N(二) 波兰和南非-2 中文版" ./d nv-b2 https://www.youtube.com/watch?v=dzR4mg1vAhc
+fps=25 speed=1 n=1 ss=0 S1="《更强大的力量》\\\\N(二) 波兰和南非-3 中文版" ./d nv-b3 https://www.youtube.com/watch?v=4z-6h5VlwMA
+fps=25 speed=1 n=1 ss=122 S1="《更强大的力量》\\\\N(三) 丹麦和智利-1 中文版" ./d nv-c1 https://www.youtube.com/watch?v=FL9a3O7Rg0Y
+fps=25 speed=1 n=1 ss=0 S1="《更强大的力量》\\\\N(三) 丹麦和智利-2 中文版" ./d nv-c2 https://www.youtube.com/watch?v=Vx4rpb1NJtg
+fps=25 speed=1 n=1 ss=0 S1="《更强大的力量》\\\\N(三) 丹麦和智利-3 中文版" ./d nv-c3 https://www.youtube.com/watch?v=M-Eu3CIqXXo
+
+
