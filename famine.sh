d0d3222f1b30a8ee9c8b4b341296a9d022d511cf
Wang Gonging <wanggonging@gmail.com>
2018-01-24 10:17:58 +0800

    famine.sh

---
+++ famine.sh
@@ -0,0 +1,23 @@
+
+#
+# Famine
+#
+
+#c=38 fps=10 speed=1 n=3 ss=0 S1="公视主题之夜 毛泽东的大饥荒 2017年11月17日" ./d mao171117 https://www.youtube.com/watch?v=Lotnpq0Usgc
+#c=35 fps=10 speed=1 n=4 ss=0 S1="1994年PBS纪录片 一个世纪的革命 （二) 1949-1976 中文字幕" ./d pbs2 https://www.youtube.com/watch?v=GMaXSKH2w3c
+#c=35 fps=10 speed=1.1 n=1 ss=431 S1="资深报人李勇 揭秘毛泽东私人医生 李志绥之死 2017.2" ./d lzs2017 https://www.youtube.com/watch?v=p5xcLhhI1j4
+c=35 fps=10 speed=1 n=2 ss=2:25 S1="1993年纪录片 四九年以后的 毛泽东 " ./d maobbc1993 https://www.youtube.com/watch?v=_TdY9oxV3jk
+#fps=10 speed=1 n=1 ss=0 S1="解密时刻 大饥荒 天灾还是人祸？ 2012年11月12日" ./d djh1 https://www.youtube.com/watch?v=KQGXZc_6ei4
+#fps=10 speed=1 n=1 ss=0 S1="解密时刻 大饥荒 死人不是数字游戏 2012年12月10日" ./d djh2 https://www.youtube.com/watch?v=GW-8Eax1LIY
+#fps=10 speed=1 n=1 ss=0 S1="解密时刻 大饥荒 谁骗谁？ 2012年12月20日" ./d djh3 https://www.youtube.com/watch?v=tGzZeaIeSBE
+#fps=10 speed=1.1 n=1 ss=0 S1="解密时刻 大饥荒 周恩来下令毁证 2013年1月13日" ./d djh4 https://www.youtube.com/watch?v=8F0b51rmacs
+#fps=10 speed=1 n=1 ss=0 S1="解密时刻 大饥荒 谁之过？ 2013年1月27日" ./d djh5 https://www.youtube.com/watch?v=-cr9SEc_KpM
+
+c=30 fps=10 speed=1 n=4 ss=0 S1="艾晓明纪录片 《夹边沟祭事》 01-右派农场\\\\N2017" ./d jbg01 https://www.youtube.com/watch?v=M0NN1F_HegY
+c=30 fps=10 speed=1 n=4 ss=0 S1="艾晓明纪录片 《夹边沟祭事》 02-劳教青春\\\\N2017" ./d jbg02 https://www.youtube.com/watch?v=2PqLOs_YMWY
+#fps=10 speed=1.1 n=4 ss=0 S1="艾晓明纪录片 《夹边沟祭事》 03-风雪明水\\\\N 2017" ./d jbg03 https://www.youtube.com/watch?v=PJwc8fNEuiY
+#fps=10 speed=1.1 n=4 ss=0 S1="艾晓明纪录片 《夹边沟祭事》 04-抢救人命\\\\N 2017" ./d jbg04 https://www.youtube.com/watch?v=wSPf2ZouudI
+#fps=10 speed=1.1 n=4 ss=0 S1="艾晓明纪录片 《夹边沟祭事》 05-魂归何处\\\\N 2017" ./d jbg05 https://www.youtube.com/watch?v=jCyLJLj3Xyw
+ 
+ 
+
