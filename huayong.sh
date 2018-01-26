abfd9374b2cb537adc9732421deac3583308b5c3
Wang Gonging <wanggonging@gmail.com>
2018-01-26 10:01:02 +0800

    huayong.sh

---
+++ huayong.sh
@@ -0,0 +1,16 @@
+
+#
+# 华涌
+#
+
+#fps=10 speed=1 n=1 ss=0 S1="华涌有话说（1） 2018-1-11 倡议“真话运动” 力挺山西临汾 真话义士常珈瑄" ./d hyszh01 https://www.youtube.com/watch?v=DXnVWloXQDY
+#fps=10 speed=1 n=1 ss=0 S1="华涌有话说（2） 2018-1-12" ./d hyszh02 https://www.youtube.com/watch?v=IoL_8HlXIXg
+#fps=10 speed=1 n=1 ss=0 S1="华涌有话说（3） 2018-1-13" ./d hyszh03 https://www.youtube.com/watch?v=c6dWdmVmKrI
+#fps=10 speed=1 n=1 ss=0 S1="华涌有话说（4） 2018-1-14 有彩蛋哦！" ./d hyszh04 https://www.youtube.com/watch?v=8fUAf1_y0gE
+#fps=10 speed=1 n=1 ss=0 S1="真话运动之 《华涌叨笔刀》5" ./d hy0117 https://www.youtube.com/watch?v=JMgJ9HIRu0o
+#fps=10 speed=1 n=1 ss=0 S1="真话运动之 《华涌叨笔刀》6 两个人的无酒饭醉" ./d hy01172 https://www.youtube.com/watch?v=iPbQPnVESMQ
+#fps=10 speed=1 n=1 ss=0 S1="真话运动之 《华涌叨笔刀》7 外滩咖啡馆\\\\N2018-1-18" ./d hy0118 https://www.youtube.com/watch?v=d55wmDQC_-I
+#c=36 fps=10 speed=1 n=1 ss=0 S1="华涌在海口 2018-1-22 巧遇艺术家艺工" ./d hy0122 https://www.youtube.com/watch?v=_0TY4n9P_6g
+#fps=10 speed=1 n=1 ss=0 S1="华涌决定再一次行动 关注农民工 为低端人口说真话！ 2018.01.23" ./d hy0123 https://www.youtube.com/watch?v=F81TKAwuskA
+#c=30 fps=10 speed=1 n=1 ss=0 S1="华涌在广州 2018年1月24日-25日" ./d hy012425 https://www.youtube.com/watch?v=YV1AeEFc488
+
