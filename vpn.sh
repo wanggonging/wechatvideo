02c2169ff5251400e4e9487e5a23ba1e7e4b5411
Wang Gonging <wanggonging@gmail.com>
2018-01-26 10:02:41 +0800

    vpn.sh

---
+++ vpn.sh
@@ -0,0 +1,22 @@
+
+#
+# VPN
+#
+
+#res=720 c=30 fps=10 speed=1 n=1 ss=0 S1="2018.01.04 darrenliuwei.com 手把手教你搭建ssr翻墙 Linux Windows MacOS 手机" ./d darrenliu20180104 https://www.youtube.com/watch?v=_6lRX9S4knI
+#cc=30 fps=10 speed=1 n=1 ss=0 S1="2018.01.18 极力推荐一款 免费翻墙插件 让你畅游YouTube 看4K不卡顿 关键是不花钱" ./d fanqiang20180118 https://www.youtube.com/watch?v=LYFNU3cOXUc
+#res=720 fps=10 speed=1 n=1 ss=0 S1="蓝视星空第98期 推荐第二款 谷歌浏览器 科学上网插件 2018.1.22" ./d vpn0122 https://www.youtube.com/watch?v=0KK6Xw_S2Lg
+
+#
+# VPNASK
+#
+
+#c=30 fps=10 speed=1 n=1 ss=0 S1="2017.08.01 VPN翻墙程序在中国区 被苹果下架 申请一个美国区Apple ID 就可以恢复正常！" ./d vpn_appid https://www.youtube.com/watch?v=EXUbgANMgNE
+#c=30 fps=10 speed=1 n=1 ss=0 S1="2017.12.07 iPhone手机如何成功翻墙？ 搬瓦工VPS自建VPN Shadowsocks翻墙软件 快速稳定！" ./d vpn171207 https://www.youtube.com/watch?v=iyIHqNm6LAg
+#c=30 fps=10 speed=1 n=1 ss=0 S1="2017.10.20 Android如何找到免费 VPN成功翻墙访问 Google Youtube和Facebook" ./d vpn171020 https://www.youtube.com/watch?v=WGgTQHmbcGc
+#c=30 fps=10 speed=1 n=1 ss=0 S1="2017.10.17 iPhone手机该如何 成功翻墙访问Google Youtube和Facebook" ./d vpn171017 https://www.youtube.com/watch?v=Z37PoZ4ho8c
+#c=30 fps=10 speed=1 n=1 ss=0 S1="2017.09.14 iPhone手机用户该 如何成功翻墙访问 Google Youtube和Facebook" ./d vpn170914 https://www.youtube.com/watch?v=2Txysz7-06g
+#c=30 fps=10 speed=1 n=1 ss=0 S1="2017.08.28 免费翻墙工具Opera VPN推荐 免费访问Google Youtube和Facebook" ./d vpn0828 https://www.youtube.com/watch?v=CVko5OtopYw
+#c=30 fps=10 speed=1 n=1 ss=0 S1="2017.08.15 ExpressVPN在Android上 如何成功访问 Google Youtube和Facebook？" ./d vpn170815 https://www.youtube.com/watch?v=63Hka-eXAtc
+#c=30 fps=10 speed=1 n=1 ss=0 S1="2017.08.07 如何通过支付宝 购买NordVPN翻墙服务？" ./d vpn170807 https://www.youtube.com/watch?v=TD2l9V411BA
+
