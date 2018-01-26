375e5c953eb1cf419a66190a64c38a2c4984fa18
Wang Gonging <wanggonging@gmail.com>
2018-01-26 10:02:10 +0800

    gwg.sh

---
+++ gwg.sh
@@ -0,0 +1,15 @@
+
+
+#
+# GWG
+#
+
+#fps=11 speed=1 n=1 ss=0 S1="《郭文贵爆料》 宣传片 纸牌屋配乐版" ./d gwg08091 https://www.youtube.com/watch?v=zxyYgWXWOHU
+#fps=11 speed=1 n=1 ss=0 S1="郭文贵 主题曲 战无不胜" ./d gwg08261 https://www.youtube.com/watch?v=5Dmdz1tanrc
+#fps=11 speed=1 n=1 ss=0 S1="万里长城永不倒 郭文贵" ./d gwg10924 https://www.youtube.com/watch?v=xrzjAXqVrZ4
+#fps=10 speed=1 n=1 ss=0 S1="天上宫殿与地上蝼蚁" ./d gwg08262 https://www.youtube.com/watch?v=kYfFFZB70nU
+#fps=10 speed=1 n=1 ss=0 S1="《郭文贵爆料》 宣传片 蓝金黄计划" ./d gwg08122 https://www.youtube.com/watch?v=0fyo4n4WIng
+#fps=10 speed=1 n=1 ss=0 S1="献礼Miles爆料之 《再向虎山行》" ./d gwg08211 https://www.youtube.com/watch?v=PnTxPKJhqO0
+#fps=10 speed=1 n=1 ss=0 S1="文贵伐赵" ./d gwg0708 https://www.youtube.com/watch?v=jY-0WYaCTxQ
+
+
